05d432d7e9856168fca21e73a3de2149a4776205
MhaamadAli <m.alialaawar@gmail.com>
2024-03-03 15:37:02 +0200

    created initializeGame & drawDashes

--- scripts/game.js
+++ scripts/game.js
@@ -15,12 +15,29 @@ const words = [
     "tech"
 ]
 
-const randomWord = getRandomWord(words);
+const randomWord = getRandomWord(words)
 
-
+initializeGame()
 
 // functions:
 
 function getRandomWord(wordsArray) {
-    return wordsArray[Math.floor(Math.random() * wordsArray.length)];
+    return wordsArray[Math.floor(Math.random() * wordsArray.length)]
+}
+
+
+
+function initializeGame() {
+    displayedWord = "-".repeat(randomWord.length)
+    drawDashes(displayedWord)
+}
+
+
+function drawDashes(word) {
+    const answerSection = document.getElementById("answer-section")
+    answerSection.innerHTML = ""
+
+    word.split('').forEach(character => {
+        answerSection.innerHTML += `<span>${character}</span>`
+    })
 }
